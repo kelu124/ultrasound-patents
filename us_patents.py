dea6d1e2ac1a291cfbc28c784d9f9928d00ea360
Kelu <kelu124@gmail.com>
2018-05-06 20:42:00 +0200

    Adding new patents

--- us_patents.py
+++ us_patents.py
@@ -20,10 +20,64 @@ __author__      = "kelu124"
 __copyright__   = "Copyright 2018, Kelu124"
 __license__ 	= "cc-by-sa/4.0/"
 
-ListOfPatents =["US3918024","US4413629","US4159462","US5295485","US3919683","US4075598","US4030343","US3918025","US3419845","US3166731","US4241608","US3864660","US3911730","US3805596","US3820387","US3693415","US3675472","US3789833","US4253338","US4319489","US4030343"]
+ListOfPatents =[]
+
+
+ListOfPatents.append("US3805596A")
+ListOfPatents.append("US4092867A")
+ListOfPatents.append("US3406564A")
+ListOfPatents.append("US3690311A")
+ListOfPatents.append("US3721227A")
+
+ListOfPatents.append("US3955561A")
+ListOfPatents.append("US3990300A")
+ListOfPatents.append("US4200885A")
+ListOfPatents.append("US4092867A")
+ListOfPatents.append("US4184094A")	
+
+ListOfPatents.append("FR2516272A1")	
+ListOfPatents.append("US4274421A")	
+ListOfPatents.append("US4418698A")	
+ListOfPatents.append("EP0142832A2")	
+ListOfPatents.append("EP0186231A2")	
+ListOfPatents.append("US3690311A")	
+
+ListOfPatents.append("US3086390A")	
+ListOfPatents.append("US3815409A")	
+ListOfPatents.append("US3777740A")	
+ListOfPatents.append("US3969926A")	
+ListOfPatents.append("US4316390A")	
+
+ListOfPatents.append("FR2292457A1")	
+ListOfPatents.append("DE2919024A1")	
+ListOfPatents.append("US6106471A")	
+ListOfPatents.append("DE2709991A1")	
+ListOfPatents.append("DE3008553A1")	
+ListOfPatents.append("EP0090567A1")	
+ListOfPatents.append("US4010634A")	
+ListOfPatents.append("US4337661A")
+
+ListOfPatents.append("US3480002A")	
+ListOfPatents.append("DE2719130A1")	
+ListOfPatents.append("US3990300A")	
+ListOfPatents.append("US4200885A")	
+ListOfPatents.append("US4274421A")	
+ListOfPatents.append("EP0142832A2")	
+	
+
+ListOfPatents.append("EP0186231A2")	
+ListOfPatents.append("US4515165A")	
+ListOfPatents.append("US4141347A")	
+ListOfPatents.append("DE3214740A1")	
+
+
 
 MaDB = "patents.db"
 check_or_create_all_tables(MaDB)
 for Patent in ListOfPatents:
 	result = get_soup(Patent,MaDB)
 
+if 0:
+	BackWards = get_top_back(MaDB)
+	for Patent in BackWards:
+		result = get_soup(Patent[0],MaDB)
